1351de850deded9409a490af03788ad2bf05fc83
Sheikh R <152389191+sak3y@users.noreply.github.com>
2026-08-03 22:37:58 +0100

    Add: same-binary-tree - submission-1

---
+++ Data Structures & Algorithms/same-binary-tree/submission-1.java
@@ -0,0 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        Stack<TreeNode> s1 = new Stack<>();
+        Stack<TreeNode> s2 = new Stack<>();
+
+        s1.push(p);
+        s2.push(q);
+
+        while (!s1.isEmpty() && !s2.isEmpty()) {
+            TreeNode curr1 = s1.pop();
+            TreeNode curr2 = s2.pop();
+            
+            if (curr1 == null && curr2 == null) continue;
+            if (curr1 == null || curr2 == null) return false;
+            if (curr1.val != curr2.val) return false;
+
+            s1.push(curr1.left);
+            s1.push(curr1.right);
+            s2.push(curr2.left);
+            s2.push(curr2.right);
+        }
+
+        return s1.isEmpty() && s2.isEmpty();
+    }
+}
+
+/*
+    Compare trees to see if they are equal
+
+    A tree is equal if it has the:
+    same nodes
+    same values 
+    in the same place
+
+    The idea is
+    Have two pointer that look at each node. Check values
+    Then they follow the general structure
+
+    DFS
+    - we could use a stack that pushes values in the tree.
+    - We pop the value
+    - Compare it's equal
+    - Then push it's children 
+    - Pop
+    - Check it's equal
+    - End when it's becomes null
+
+    Recursion
+    - base case: if the tree is null return true
+    - If values are equal
+
+*/
