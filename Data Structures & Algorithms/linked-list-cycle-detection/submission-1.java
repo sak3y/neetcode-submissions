e32531d233fcbab89606f471143f3cd86866ca14
Sheikh R <152389191+sak3y@users.noreply.github.com>
2026-08-05 22:10:59 +0100

    Add: linked-list-cycle-detection - submission-1

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-1.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+
+        while (head != null) {
+            if (!set.add(head)) return true;
+            head = head.next;
+        }
+        return false;
+    }
+}
+
+
+/*
+    Look for a cycle in a linkedLIst
+    Cycle => if at any point, a node can be revisited. We would say that's a cycle.
+    Essentiatily and infinite pointer where the next pointer point to an existing node.
+    The opposite is the case, when a pointer can point to null and we can be done parsing the list
+
+    How do we deterimine a cycle?
+    - If at some point the values start repeating, (and we keep track), then we know 
+        well not really, in a really long list, values may repeat by chance so we can't use values to decide
+    - what if we used two pointer
+        a fast pointer and a slow pointer
+        a slow pointer catching up to the fast indicates that we reached a loop right?
+
+        how would we know that it reached a loop and they 
+    
+*/
