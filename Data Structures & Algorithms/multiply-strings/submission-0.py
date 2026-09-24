70914467273359314ca820092eaa7030161025da
Sheikh R <152389191+sak3y@users.noreply.github.com>
2026-09-24 11:56:57 +0100

    Add: multiply-strings - submission-0

---
+++ Data Structures & Algorithms/multiply-strings/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        a = int(num1)
+        b = int(num2)
+
+        return str(a * b)
+
+
+"""
+    String -> int -> multiple wiht another int -> convert to string
+    
+    NO LIBRARIES
+
+    Conerting an string to an int:
+
+"""
